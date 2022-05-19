801120bc582120856d733764f76910faf58a8085
MatheusSenac <matheussanches1104@gmail.com>
2022-05-19 19:57:04 -0300

    first commit

---
+++ IngressoMVC/Models/Artista.cs
@@ -0,0 +1,29 @@
+﻿using IngressoMVC.Models;
+using SistemaStreaming.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaStreaming.Models
+{
+    public abstract class Artista : IEntidade
+    {
+        protected Artista(int id, string nome, string bio, string fotoPerfilURL)
+        {
+            this.id = id;
+            DataCadastro = DateTime.Now;
+            DataAlteracao = DataCadastro;
+            Nome = nome;
+            Bio = bio;
+            FotoPerfilURL = fotoPerfilURL;
+        }
+
+        public int id { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public string Nome { get; private set; }
+        public string Bio { get; private set; }
+        public string FotoPerfilURL { get; private set; }
+    }
+}
---
+++ IngressoMVC/Models/AtorFilme.cs
@@ -0,0 +1,18 @@
+﻿using IngressoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaStreaming.Models
+{
+    public class AtorFilme
+    {
+        public int AtorId { get; set; }
+        public Ator Ator { get; set; }
+
+        public int FilmeId { get; set; }
+        public Filme Filme { get; set; }
+    }
+}
